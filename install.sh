8bcac8053f2f747c9f31dd682995a4b6b083f387
new0288a <44078856+new0288a@users.noreply.github.com>
2024-02-01 14:33:35 +0800

    Update install.sh
    switch to context docker-desktop at the beginning

--- install.sh
+++ install.sh
@@ -18,7 +18,11 @@ sudo apt-get install -y \
   curl \
   wget
 
+# install vcluster
 curl -L -o vcluster "https://github.com/loft-sh/vcluster/releases/latest/download/vcluster-linux-amd64" && sudo install -c -m 0755 vcluster /usr/local/bin && rm -f vcluster
+
+# switch to context docker-desktop at the beginning
+kubectl config use-context docker-desktop 2>/dev/null || true
 
 cat <<'EOF' >> ~/.bashrc
 
